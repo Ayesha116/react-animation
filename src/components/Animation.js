9e0dcb389d289a8cd22199c90dba69393827baec
Ayesha116 <ayeshajawed1168@gmail.com>
2020-08-20 19:25:28 +0500

    error resolving

--- src/components/Animation.js
+++ src/components/Animation.js
@@ -24,7 +24,7 @@ export const Animation = ()=> {
     return(
         <div class = "wrapper">
             <div className = "sky">
-                <a className = "sun" href="https://www.animatedimages.org/cat-sun-278.htm"><img  src="https://www.animatedimages.org/data/media/278/animated-sun-image-0268.gif" border="0" alt = "" /></a>
+                <a className = "sun" href="https://www.animatedimages.org/cat-sun-278.htm"><img  src="https://www.animatedimages.org/data/media/278/animated-sun-image-0268.gif" border="0" alt = "sun" /></a>
                 <div ref = { cloud.ref }>
                     <img className = "c0" src = { c3 } width= "100px" alt = ""></img>
                     <img className = "c1" src = { c3 } width= "100px" alt = ""></img>
@@ -34,13 +34,13 @@ export const Animation = ()=> {
             </div>
             <div className = "grass">
                 
-                <img className = "house"  src = { house } width = "0px"></img>
+                <img className = "house"  src = { house } width = "0px" alt = "house"></img>
                 <a className = "chicken" href="https://www.animatedimages.org/cat-chickens-532.htm"><img  ref = { chicken.ref } src="https://www.animatedimages.org/data/media/532/animated-chicken-image-0079.gif" border="0" alt="" /></a>
                 
             </div>
-            <a href="https://www.animatedimages.org/cat-butterflies-291.htm"><img className = "butterfly" src="https://www.animatedimages.org/data/media/291/animated-butterfly-image-0082.gif" border="0" alt="animated-butterfly-image-0082" /></a>
-            <img className = "tree" src = { tree } width = "450px"></img>
-            <a href="https://www.animatedimages.org/cat-cats-209.htm"><img className = "cat" width ="90px" src="https://www.animatedimages.org/data/media/209/animated-cat-image-0072.gif" border="0" alt="animated-cat-image-0072" /></a>
+            <a href="https://www.animatedimages.org/cat-butterflies-291.htm"><img className = "butterfly" src="https://www.animatedimages.org/data/media/291/animated-butterfly-image-0082.gif" border="0" alt = "butterflies" /></a>
+            <img className = "tree" src = { tree } width = "450px" alt = "tree" ></img>
+            <a href="https://www.animatedimages.org/cat-cats-209.htm"><img className = "cat" width ="90px" src="https://www.animatedimages.org/data/media/209/animated-cat-image-0072.gif" border="0" alt="cat" /></a>
             
             
         </div>
