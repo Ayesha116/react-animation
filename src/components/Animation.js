7dc01f517269a0ff9772320f888f62ecfea5d245
Ayesha116 <ayeshajawed1168@gmail.com>
2020-08-20 18:53:17 +0500

    deleted extra images and comments

--- src/components/Animation.js
+++ src/components/Animation.js
@@ -2,7 +2,7 @@ import React from 'react';
 import useWebAnimations from "@wellyshen/use-web-animations";
 import './style.css';
 import tree from "../images/tree.png"
-import cycle from "../images/cycle.png"
+
 import c3 from "../images/c3.png";
 import house from "../images/house.png"
 
@@ -19,11 +19,6 @@ export const Animation = ()=> {
                 duration: 5500,
                 } })
 
-        var bird = useWebAnimations({keyframes : { transform : ["translateX(0px)" , "translateX(500px)"] },
-                timing:{
-                iterations:Infinity,
-                duration: 10000,
-                } })
 
 
     return(
@@ -35,8 +30,6 @@ export const Animation = ()=> {
                     <img className = "c1" src = { c3 } width= "100px" alt = ""></img>
                     <img className = "c2" src = { c3 } width= "100px" alt = ""></img>
                 </div>
-                {/* <a   href="https://www.animatedimages.org/cat-birds-230.htm" ><img className = "bird" ref = { bird.ref } src="https://www.animatedimages.org/data/media/230/animated-bird-image-0132.gif" width = "50px" border="0" alt="animated-bird-image-0132" /></a> */}
-                {/* <a href="https://www.animatedimages.org/cat-birds-230.htm"><img src="https://www.animatedimages.org/data/media/230/animated-bird-image-0417.gif" border="0" alt="animated-bird-image-0417" /></a> */}
 
             </div>
             <div className = "grass">
@@ -47,7 +40,6 @@ export const Animation = ()=> {
             </div>
             <a href="https://www.animatedimages.org/cat-butterflies-291.htm"><img className = "butterfly" src="https://www.animatedimages.org/data/media/291/animated-butterfly-image-0082.gif" border="0" alt="animated-butterfly-image-0082" /></a>
             <img className = "tree" src = { tree } width = "450px"></img>
-            {/* <a href="https://www.animatedimages.org/cat-crows-1754.htm"><img className = "crow" width = "250px" src="https://www.animatedimages.org/data/media/1754/animated-crow-image-0015.gif" border="0" alt="animated-crow-image-0015" /></a> */}
             <a href="https://www.animatedimages.org/cat-cats-209.htm"><img className = "cat" width ="90px" src="https://www.animatedimages.org/data/media/209/animated-cat-image-0072.gif" border="0" alt="animated-cat-image-0072" /></a>
             
             
